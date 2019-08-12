ec039a83ed257fd368ac0bad2e1ac3cd3c0361b0
Roman <vazhnik.roma@mail.ru>
2019-08-12 17:28:38 +0300

    change some logic

--- HW_6/ListTableViewController.swift
+++ HW_6/ListTableViewController.swift
@@ -15,7 +15,7 @@ class ListTableViewController: UITableViewController {
     var phoneNumbersArray = ["1405122", "5670133", "1256200", "9563833", "4510432"]
     var emailsArray = ["aaa@gmail.ru", "bbb@gmail.ru", "ссс@gmail.ru", "ddd@gmail.ru", "fff@gmail.ru"]
     
-    var personsArray: [Person] = []
+    var persons: [Person] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,17 +28,18 @@ class ListTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return personsArray.count
+        return persons.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "listTableViewCell", for: indexPath)
         
-        let currentPerson =  personsArray[indexPath.row]
+        let currentPerson =  persons[indexPath.row]
         cell.textLabel?.text = "\(currentPerson.name) \(currentPerson.surname)"
         return cell
     }
     
+    //Cell setup
     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         cell.textLabel?.font = UIFont(name: "Kailasa", size: 18)
         cell.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
@@ -48,34 +49,25 @@ class ListTableViewController: UITableViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let vc = segue.destination as! DetailViewController
         if let indexPath = tableView.indexPathForSelectedRow {
-            vc.sourceStructure = personsArray[indexPath.row]
+            let vc = segue.destination as! DetailViewController
+            vc.sourceStructure = persons[indexPath.row]
         }
     }
     
-    private func getRandomNumberFromArray(_ range: Int) -> Int {
-        let randomInt = Int.random(in: 0..<range)
-        return randomInt
-    }
-
     private func createRandomStructure(countOfIterations: Int) {
-        for _ in 0..<countOfIterations {
+        namesArray.shuffle()
+        surnamesArray.shuffle()
+        phoneNumbersArray.shuffle()
+        emailsArray.shuffle()
+        for element in 0..<namesArray.count {
             
-            let range = namesArray.count
-            
-            let randomNumberForName = getRandomNumberFromArray(range)
-            let randomNumberForSurname = getRandomNumberFromArray(range)
-            let randomNumberForPhoneNumber = getRandomNumberFromArray(range)
-            let randomNumberForEmail = getRandomNumberFromArray(range)
-            
-            let name = namesArray.remove(at: randomNumberForName)
-            let surname = surnamesArray.remove(at: randomNumberForSurname)
-            let phoneNumber = phoneNumbersArray.remove(at: randomNumberForPhoneNumber)
-            let email = emailsArray.remove(at: randomNumberForEmail)
-            
+            let name = namesArray[element]
+            let surname = surnamesArray[element]
+            let phoneNumber = phoneNumbersArray[element]
+            let email = emailsArray[element]
             let newPerson = Person(name: name, surname: surname, phoneNumber: phoneNumber, email: email)
-            personsArray.append(newPerson)
+            persons.append(newPerson)
             
         }
     }
